9e7c8ed5235f4992e41c849d7f1b14dde703a271
renatka <renatka@mail.ru>
2025-12-11 12:51:08 +0300

    Release 1.4 Attempt to execute edmo exercise on SberCode with podman

--- src/test/java/ru/securecodereview/jdbc/dao/ClientDaoJdbcTest.java
+++ src/test/java/ru/securecodereview/jdbc/dao/ClientDaoJdbcTest.java
@@ -1,5 +1,8 @@
 package ru.securecodereview.jdbc.dao;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.testcontainers.containers.BindMode;
@@ -9,6 +12,7 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import ru.securecodereview.core.DataSourceImpl;
 import ru.securecodereview.core.dao.ClientDao;
 import ru.securecodereview.core.model.Client;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,9 +20,17 @@ import java.util.List;
 import java.util.Properties;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
 
 @Testcontainers
 class ClientDaoJdbcTest {
+
+    ClientDao clientDao;
+
+    @BeforeEach
+    void setUp(){
+        clientDao = new ClientDaoJdbc(new DataSourceImpl(getConnectionProperties(), postgresqlContainer.getJdbcUrl()));
+    }
 
 
     @Container
@@ -29,12 +41,19 @@ class ClientDaoJdbcTest {
             .withClasspathResourceMapping("00_createTables.sql", "/docker-entrypoint-initdb.d/00_createTables.sql", BindMode.READ_ONLY)
             .withClasspathResourceMapping("01_insertData.sql", "/docker-entrypoint-initdb.d/01_insertData.sql", BindMode.READ_ONLY);
 
-@ParameterizedTest(name = "SQL Injection")
-@ValueSource(strings = {"name2", "injection\' OR 1 = 1 --"})
-void selectTest(String search) {
-        ClientDao clientDao = new ClientDaoJdbc(new DataSourceImpl(getConnectionProperties(), postgresqlContainer.getJdbcUrl()));
-        List<Client> clientList = clientDao.findByName(search);
+
+    @Test
+    @DisplayName("Test with name2")
+    void selectName2Test(){
+        List<Client> clientList = clientDao.findByName("name2");
         assertTrue(clientList.contains(new Client(2, "name2")));
+    }
+
+    @Test
+    @DisplayName("Test with injection")
+    void selectInjectionTest(){
+        List<Client> clientList = clientDao.findByName("injection\' OR 1 = 1 --");
+        assertFalse(clientList.contains(new Client(2, "name2")));
     }
 
     private Properties getConnectionProperties() {
